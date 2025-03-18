e5240d182556ab98e819316bff7220b4fb885d62
@vcastro-dev <vcastro2395@gmail.com>
2025-03-17 21:58:13 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
